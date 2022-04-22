50451792aba6d8c53d0192ca8eac8dfa0f896e01
Jérémy Dillenbourg <jdi@bepark.eu>
2022-04-22 15:44:30 +0200

    Keep only the essential

---
+++ APrivacy/Izix.php
@@ -0,0 +1,22 @@
+<?php
+
+class Izix
+{
+	public float $price;
+}
+
+/*class Izix
+{
+	private float $price;
+}*/
+
+/*class Izix
+{
+	public readonly float $price;
+}*/
+
+//What's the risk  ?
+
+$myClass = new Izix();
+
+var_dump($myClass);
